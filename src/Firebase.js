af9b37d4009dd05c053a5c67666473c2bac789cc
Apurv gupta <56751664+apurvgupta-ops@users.noreply.github.com>
2022-05-02 14:43:30 +0530

    Add Firebase Config file

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDUXTtDpdf808JsX2MbKcZ_G9WKTMJovho",
+    authDomain: "wetalk-2c171.firebaseapp.com",
+    projectId: "wetalk-2c171",
+    storageBucket: "wetalk-2c171.appspot.com",
+    messagingSenderId: "587669479921",
+    appId: "1:587669479921:web:0ca45d0f3b0aa9cdd0a450"
+  };
